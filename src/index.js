dc13c2fa2b4255b9bb15aade33b2c73006600061
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-09-08 22:24:44 +0500

    first

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // Импортируем createRoot
+import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
+import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService
+	.getAllCharacters()
+	.then((res) => res.data.results.forEach((item) => console.log(item.name)));
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
