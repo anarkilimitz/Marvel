fd0cb111388fe2a3c03a807a4e431f2a0ae9c65b
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-10-06 22:45:36 +0500

    react-lazy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,17 @@
+import { lazy, Suspense } from 'react';
+
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import AppHeader from '../../components/appHeader/AppHeader';
-import MainCharPage from '../pages/MainCharPage';
-import ComicsPage from '../pages/ComicsPage';
-import Page404 from '../pages/404/Page404';
-import SingleComicPage from '../pages/singleComicPage/SingleComicPage';
+import Spinner from '../spinner/Spinner';
+
+const Page404 = lazy(() => import('../pages/404/Page404'));
+const MainCharPage = lazy(() => import('../pages/MainCharPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() =>
+	import('../pages/singleComicPage/SingleComicPage')
+);
 
 const App = () => {
 	const [selectedChar, setChar] = useState(null);
@@ -18,20 +25,22 @@ const App = () => {
 			<div className="app">
 				<AppHeader />
 				<main>
-					<Routes>
-						<Route
-							path="/"
-							element={
-								<MainCharPage
-									onCharSelected={onCharSelected}
-									selectedChar={selectedChar}
-								/>
-							}
-						/>
-						<Route path="/comics" element={<ComicsPage />} />
-						<Route path="/comics/:comicId" element={<SingleComicPage />} />
-						<Route path="*" element={<Page404 />} />
-					</Routes>
+					<Suspense fallback={<Spinner />}>
+						<Routes>
+							<Route
+								path="/"
+								element={
+									<MainCharPage
+										onCharSelected={onCharSelected}
+										selectedChar={selectedChar}
+									/>
+								}
+							/>
+							<Route path="/comics" element={<ComicsPage />} />
+							<Route path="/comics/:comicId" element={<SingleComicPage />} />
+							<Route path="*" element={<Page404 />} />
+						</Routes>
+					</Suspense>
 				</main>
 			</div>
 		</Router>
